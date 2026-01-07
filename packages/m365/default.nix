cf136f5c680f997cda94599b9e8d5f19394880be
Guillaume Friloux <gfriloux@datasolution.fr>
2026-01-07 08:55:24 +0100

    fix(packages): ;
    ;
    
    Signed-off-by: Guillaume Friloux <gfriloux@datasolution.fr>

--- packages/m365/default.nix
+++ packages/m365/default.nix
@@ -4,11 +4,12 @@ python3Packages.buildPythonApplication {
   pname = "m365-refresh";
   src = ./src;
 
-  propagatedBuildInputs = with python3Packages [
+  propagatedBuildInputs = with python3Packages; [
     msal
   ];
 
   format = "other";
+
   installPhase = ''
     mkdir -p $out/bin
     install -m755 m365-refresh.py $out/bin/
@@ -17,5 +18,5 @@ python3Packages.buildPythonApplication {
   meta = with lib; {
     description = "Refresh oauth2 access token using MSAL";
     platforms = platforms.linux;
-  }
+  };
 }
