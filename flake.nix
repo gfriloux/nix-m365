86aaaa7143450a5d973b2af1c7e38da6d6d88c96
Guillaume Friloux <gfriloux@datasolution.fr>
2026-01-07 08:40:31 +0100

    feat(all): add flake.nix
    add flake.nix
    
    Signed-off-by: Guillaume Friloux <gfriloux@datasolution.fr>

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      src = ./.;
+
+      snowfall = {
+        namespace = "m365";
+      };
+      alias = {
+        packages.default = "m365";
+      };
+    };
+
+}
